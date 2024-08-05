9fbb2d763ba571176bca0b7d55872fb12d78a7bd
Kushagra Singhal <74611061+kushagra21-afk@users.noreply.github.com>
2024-08-05 13:25:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,25 @@ mongoose.connect(
     
   }
 );
+const bcrypt = require("bcrypt");
+//registeration using bcrypt for security 
+app.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+    res.status(200).json("user has been created succesfully please login with the same credentials");
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
