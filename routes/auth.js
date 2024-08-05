bbc675cf8d9e4a91a6626d7ed3fa58db19125b07
Kushagra Singhal <74611061+kushagra21-afk@users.noreply.github.com>
2024-08-05 17:57:46 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -12,6 +12,7 @@ router.post("/register", async (req, res) => {
       username: req.body.username,
       email: req.body.email,
       password: hashedPassword,
+      rollNumber: req.body.rollNumber,
     });
 
     const user = await newUser.save();
